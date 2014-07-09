5862089664f47f1cecbc8f928a4e9b680653fa59
Patric Boscolo <patbosc@outlook.com>
2014-07-09 12:03:48 +0200

    setting up project + installer

---
+++ UdpAzure/UdpAzureService/UdpServer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdpAzureService
+{
+    public partial class UdpServer : ServiceBase
+    {
+        public UdpServer()
+        {
+            InitializeComponent();
+            if (!System.Diagnostics.EventLog.SourceExists("UdpEventLog"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource("UdpEventLog","AzureUdpLog");
+            }
+            UdpEventLog.Source = "UdpEnventLog";
+            UdpEventLog.Log = "AzureUdpLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            UdpEventLog.WriteEntry("Starting Udp Service "+DateTime.Now);
+        }
+
+        protected override void OnStop()
+        {
+            UdpEventLog.WriteEntry("Stopping Udp Service "+DateTime.Now);
+        }
+
+        protected override void OnShutdown()
+        {
+            UdpEventLog.WriteEntry("Service Shutdown "+DateTime.Now);
+            base.OnShutdown();
+        }
+
+    }
+}
